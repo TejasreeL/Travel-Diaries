6170ded82627456937b5af45f9bc6135c9c7927c
Upasna <upasnasingh2010@outlook.com>
2023-11-04 17:32:34 +0530

    User Registration done; form validation still left

--- travelDiaries/member_auth/views.py
+++ travelDiaries/member_auth/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def member_login(request):
@@ -20,7 +21,57 @@ def member_login(request):
         return render(request, 'member_auth/member_login.html')
 
 def member_register(request):
-    return render(request, 'member_auth/member_register.html')
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+
+        if User.objects.filter(username=username).exists():
+            messages.success(request, "Username already exists, Try again!")
+            return redirect('member-register')    
+
+        my_user = User.objects.create_user(username=username, password=password)
+        my_user.first_name = fname
+        my_user.last_name = lname
+        my_user.save()
+        messages.success(request, "registered!")
+        login(request, my_user)
+        return redirect('user-home')
+        
+    
+    else:
+        return render(request, 'member_auth/member_register.html')
+
+
+# def member_register(request):
+#     if request.method == 'POST':
+#         try:
+#             username = request.POST["username"]
+#             password = request.POST["password"]
+#             fname = request.POST["fname"]
+#             lname = request.POST["lname"]
+
+#             if User.objects.filter(username=username).exists():
+#                 messages.success(request, "User already exists, Login instead!")
+#                 return redirect('member-register')
+            
+#             my_user = User.objects.create_user(username=username, password=password)
+#             my_user.first_name = fname
+#             my_user.last_name = lname
+#             my_user.save()
+
+#             messages.success(request, "Registered successfully!")
+#             login(request, my_user)
+#             return redirect('user-home')
+        
+#         except KeyError:
+#             messages.error(request, "Missing required fields")
+#             return redirect('member-register')
+
+#     else:
+#         return render(request, 'member_auth/member_register.html')
+
 
 def member_logout(request):
     logout(request)
